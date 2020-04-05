6a6de1cb61952cfd3d25474b5e313d056fc9b7c8
Roberto Salim <salimroberto95@gmail.com>
2020-04-05 20:01:51 +0800

    done mi_card

--- lib/main.dart
+++ lib/main.dart
@@ -7,32 +7,78 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var column = Column();
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.deepOrange.shade600,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100.0,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/roberto.jpeg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.yellow,
+              Text(
+                'Roberto Salim',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.deepOrange.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.deepOrange.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.add_shopping_cart,
+                    color: Colors.deepOrange.shade600,
                   ),
-                ],
+                  title: Text(
+                    '+60 19 359 1977',
+                    style: TextStyle(
+                      color: Colors.deepOrange.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
               ),
-              Container(
-                width: 100.0,
-                color: Colors.blue,
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.deepOrange.shade600,
+                  ),
+                  title: Text(
+                    'salimroberto95@gmail.com',
+                    style: TextStyle(
+                      color: Colors.deepOrange.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
@@ -41,3 +87,23 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// Row(
+//                   children: <Widget>[
+//                     Icon(
+//                       Icons.email,
+//                       color: Colors.teal,
+//                     ),
+//                     SizedBox(
+//                       width: 10.0,
+//                     ),
+//                     Text(
+//                       'salimroberto95@gmail.com',
+//                       style: TextStyle(
+//                         color: Colors.teal.shade900,
+//                         fontFamily: 'Source Sans Pro',
+//                         fontSize: 20.0,
+//                       ),
+//                     ),
+//                   ],
+//                 ),
